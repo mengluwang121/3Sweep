976a27a705829b7ea7dc27425b12c2e8ec9dfb2e
Menglu Wang <mengluwang121@hotmail.com>
2017-04-03 22:35:53 -0400

    build geometry

--- 3Sweep/ThreeSweepCmd.cpp
+++ 3Sweep/ThreeSweepCmd.cpp
@@ -1,5 +1,7 @@
 #include "ThreeSweepCmd.h"
-
+#include <maya/MSelectionList.h>
+#include <maya/MFnDependencyNode.h>
+#include <maya/MFnNurbsCurve.h>
 
 const char *pointFlag = "-p", *pointLongFlag = "-point";
 const char *modeFlag = "-m", *modeLongFlag = "-mode";
@@ -40,9 +42,6 @@ MStatus ThreeSweepCmd::doIt(const MArgList& args)
 	int mode = 0;
 	int nCurves = 0;
 
-	MArgList pointList;
-	std::vector<vec3> points;
-	
 	MArgDatabase argData(newSyntax(), args);
 
 	if (argData.isFlagSet(modeFlag))
@@ -52,10 +51,11 @@ MStatus ThreeSweepCmd::doIt(const MArgList& args)
 		argData.getFlagArgument(nCurvesFlag, 0, nCurves);
 
 	int numPoints = 0;
-	
+	MArgList pointList;
+	std::vector<vec3> points;
+
 	if (argData.isFlagSet(pointFlag)) {
 		numPoints = argData.numberOfFlagUses(pointFlag);
-		
 		for (int i = 0; i < numPoints; i++) {
 			unsigned index = 0;
 			argData.getFlagArgumentList(pointFlag, i, pointList);
@@ -71,13 +71,13 @@ MStatus ThreeSweepCmd::doIt(const MArgList& args)
 
 	if (!manager) manager = new Manager();
 
-	
 	if (manager->number_of_strokes % 3 == 0) manager->end();
 	manager->number_of_strokes = nCurves;
+
 	//more control
 	vec3 camera = vec3(0.0, 0.0, -1.0);
 	for (int i = 0; i < numPoints; i++) {
-		manager->update(points[i], camera, true);//?????????
+		manager->update(points[i], camera, true);
 	}
 
 	Circle* result = (Circle*)(manager->curt_solution->curt);
@@ -88,42 +88,71 @@ MStatus ThreeSweepCmd::doIt(const MArgList& args)
 	}
 
 	if (nCurves % 3 == 2) {
+		MGlobal::displayInfo("Circle is computed");
 		drawCircle(result, nCurves);
 	}else if (nCurves % 3 == 0) {
 
+		MGlobal::displayInfo("Extruding");
+
 		int index = (nCurves -1) / 3; //circle index
-
-		//get right curve
 		MString thirdStroke = "curve";
-		thirdStroke += nCurves;
+		thirdStroke += nCurves;//the third curve name
 
-		//build temp circle:?????????? need to be changed to build up circle set.
-		vec3 normal = result->getNormal();
-		float radius = result->getRadius();
-		MString cmd ="string $temp[] =`circle -c 0 0 0 -nr ";
-		/*cmd += normal.x;
-		cmd += " ";
-		cmd += -normal.y;
-		cmd += " ";
-		cmd += normal.z;*/
-		cmd += "0 0 -1";
-		cmd += " -r ";
-		cmd += radius;
-		cmd += "`";
-		MGlobal::displayInfo(cmd);
-		MGlobal::executeCommand(cmd, true);
-		//store the circle
-		MString circle = "$circleSet[";
-		circle += index;
-		circle += "]";
-		cmd = circle + "=$temp[0];";
-		MGlobal::displayInfo(cmd);
-		MGlobal::executeCommand(cmd, true);
+		//build temp circle
+		vec3 lastNormal = result->getNormal();
+		float lastRadius = result->getRadius();
 
-		//extrude
-		cmd = "extrude -ch true -rn false -po 1 -et 2 -ucp 1 -fpt 1 -upn 1 -rotation 0 -scale 1 -rsp 1 "+circle+" "+ thirdStroke+";";
-		MGlobal::displayInfo(cmd);
-		MGlobal::executeCommand(cmd, true);
+		for (int i = 1; i < numPoints; i++) {
+			
+			float curRadius = lastRadius*0.98;//to do
+			float scale = curRadius/ lastRadius;
+			vec3 start = points[i - 1];
+			vec3 end = points[i];
+	
+			//build start circle and curveSegment
+			MString cmd ="circle -c 0 0 0 -nr ";
+			cmd += lastNormal.x;
+			cmd += " ";
+			cmd += -lastNormal.y;
+			cmd += " ";
+			cmd += 0;
+			cmd += " -r ";
+			cmd += lastRadius;
+			cmd += " -n startCircle;";
+
+			cmd += "curve - d 1 -p ";
+			cmd += start.x;
+			cmd += " ";
+			cmd += start.y;
+			cmd += " ";
+			cmd += start.z;
+			cmd += " -p ";
+			cmd += end.x;
+			cmd += " ";
+			cmd += end.y;
+			cmd += " ";
+			cmd += end.z;
+			cmd += " -n curveSeg;";
+			MGlobal::displayInfo(cmd);
+			MGlobal::executeCommand(cmd, true);
+
+			//extrude
+			cmd = "extrude -ch true -rn false -po 1 -et 2 -ucp 1 -fpt 1 -upn 1 -rotation 0 -scale ";
+			cmd += scale;
+			cmd += " -rsp 1 startCircle curveSeg";
+			MGlobal::displayInfo(cmd);
+			MGlobal::executeCommand(cmd, true);
+
+			//reset
+			cmd = "delete startCircle; delete curveSeg;";
+			MGlobal::displayInfo(cmd);
+			MGlobal::executeCommand(cmd, true);
+
+			//update for next iteration
+			vec3 curNormal = vec3(0, 1, 0);//to do
+			lastNormal = curNormal;
+			lastRadius = curRadius;
+		}
 	}
 
 	return MStatus::kSuccess;
@@ -160,4 +189,47 @@ void ThreeSweepCmd::drawCircle(Circle* result, int numStrokes) {
 	cmd += "] = $circleFullName[0]";
 	MGlobal::displayInfo(cmd);
 	MGlobal::executeCommand(cmd, true);
+}
+
+
+std::vector<vec3> getSamplePoints(int nCurves) {
+	//get sample points
+	std::vector<vec3> pointsOnCurve;
+	int nSample = 20;
+	float step = 1.0 / 20;
+	float pctParam = 0;
+
+	MString thirdStrokeShape = "curveShape";
+	thirdStrokeShape += nCurves;
+	MGlobal::selectByName(thirdStrokeShape, MGlobal::kReplaceList);
+	MSelectionList selected;
+	MGlobal::getActiveSelectionList(selected);
+
+	MStatus status;
+	// returns the i'th selected dependency node
+	MObject obj;
+	selected.getDependNode(0, obj);
+	if (obj.hasFn(MFn::kNurbsCurve)) {
+		// Attach a function set to the selected object
+		MFnNurbsCurve curveObj(obj);
+		for (int i = 0; i < nSample; i++) {
+			MPoint point;
+			status = curveObj.getPointAtParam(pctParam, point);
+			MString info;
+			info = point.x;
+			info += point.y;
+			info += point.z;
+			MGlobal::displayInfo(info);
+
+			vec3 pointVec(point.x, point.y, point.z);
+			pointsOnCurve.push_back(pointVec);
+
+			pctParam += step;
+		}
+	}
+	else {
+		MGlobal::displayInfo("not curves!");
+	}
+
+	return pointsOnCurve;
 }
